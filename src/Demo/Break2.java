3187e93b19fd108616d7eebf74b6cc0e96308147
USER <USER@USER-VAIO>
2022-03-20 21:06:46 +0530

    added additional checks..

--- src/Demo/Break2.java
+++ src/Demo/Break2.java
@@ -4,8 +4,9 @@ public class Break2 {
 
 public static void main(String args[]) {
 	for(int i=10; i<=15; i++) {
-		if (i==13)
-	   // break;
+		if (i==14)
+	   //break;
+		//else
 		continue;
 		System.out.println(i);
 	}
